aaea67ae17f86410c18dbe39e517a1e3daa1f6d1
mamun8893 <mamunurr8893@gmail.com>
2023-06-18 01:49:10 +0600

    feat:student module

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,11 +2,15 @@ import config from '../../../config'
 import ApiError from '../../../errors/ApiError'
 import { IUser } from './user.interface'
 import { User } from './user.model'
-import { generateUserId } from './user.utils'
+import { generateStudentId } from './user.utils'
 
 const createUserToDB = async (user: IUser) => {
   //auto generated incremental id
-  const id = await generateUserId()
+  const academicSemester = {
+    year: 2025,
+    code: '01',
+  }
+  const id = await generateStudentId(academicSemester)
   user.id = id
   //default password
   if (!user.password) {
